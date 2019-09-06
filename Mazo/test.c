18f4c9c9bf0d27943702e73ba40991d02e990f4d
VIHBOY <jlholmess1@gmail.com>
2019-09-06 00:45:52 -0400

    [Test]

---
+++ Mazo/test.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+int main ( int argc, char **argv )
+{
+ 	FILE *fp;
+
+ 	char buffer[100];
+
+ 	fp = fopen ( "Cartas.txt", "r" );
+
+ 	fscanf(fp, "%s" ,buffer);
+ 	printf("%s",buffer);
+
+ 	fclose ( fp );
+
+ 	return 0;
+}
