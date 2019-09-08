7524385ff3a096ed2477f82effc33c1060c2095a
VIHBOY <jlholmess1@gmail.com>
2019-09-08 01:53:17 -0300

    Upload Card generator & Pipe
    Se sube actualizacion de code para crear tarjetas y code para posibilitar el uso de pipes

--- Mazo/test.c
+++ Mazo/test.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
+#include <string.h>
 
 
 int main ( int argc, char **argv )
 {
  	FILE *fp;
-
+  FILE *fp2;
  	char buffer[100];
-
- 	fp = fopen ( "Cartas.txt", "r" );
-
- 	fscanf(fp, "%s" ,buffer);
- 	printf("%s",buffer);
-
- 	fclose ( fp );
-
+ 	fp = fopen ("Cartas.txt","r");
+  while (fgets(buffer, 100, fp) != NULL){
+    strtok(buffer, "\n");
+    fp2= fopen(buffer,"w");
+    fclose(fp2);
+  }
+ 	  fclose (fp);
  	return 0;
 }
